938af3605bf5f89e925ce0f54d556a0aaa6544da
emilly0305 <emilly.costa.rodrigues@escola.pr.gov.br>
2025-09-25 08:01:05 -0300

    Add files via upload

---
+++ jogo.forca.py
@@ -0,0 +1,25 @@
+palavra_secreta = "girafa"
+letras_acertadas = ["_","_","_","_","_","_"]
+tentativas = 6
+
+print("Bem vindo ao jogo da forca!")
+
+while tentativas > 0 and "_" in letras_acertadas:
+    palpite = input ("Digite uma letra: "). lower()
+
+    if palpite in palavra_secreta:
+        index = 0
+        for letra in palavra_secreta:
+            if palpite == letra:
+                letras_acertadas [index] = letra
+            index +=  1
+    else:
+        tentavias -= 1
+        print(f"Você tem (tentativas) tentativas restantes.")
+       
+    print(" ".join(letras_acertadas))
+
+if "_" not in letras_acertadas:
+    print("Parabens, você ganhou!")
+else:
+    print("Que pena, você perdeu. A palavra era: ", palavra_secreta)
